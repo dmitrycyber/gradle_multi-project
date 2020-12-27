4a0912d992849bdcb3abc1a49da90a2412501a9a
Dmitry <dmitry.ivoninsky@gmail.com>
2020-12-28 00:56:44 +0300

    initial commit

---
+++ core/src/main/java/multi/project/core/Utils.java
@@ -0,0 +1,15 @@
+package multi.project.core;
+import lib.StringUtils;
+
+public class Utils {
+
+    public static boolean isAllPositiveNumbers(String... str){
+
+        for (String number : str){
+            if(!StringUtils.isPositiveNumber(number)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
